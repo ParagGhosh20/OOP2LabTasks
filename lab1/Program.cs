debaa2ec1bc4d4820408ca9dde5f733a56dccd8a
ParagGhosh20 <paragghosh2@gmail.com>
2020-10-19 10:50:57 +0600

    lab 1 done lab 2 incomplete

---
+++ lab1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello");
+            System.Console.WriteLine("Parag ghosh");
+            int x = 11;
+            int y = 11;
+            int sum = x + y;
+            Console.WriteLine(sum);
+
+            int a = 10;
+            int b = 2;
+            int Devide = a / b;
+            System.Console.WriteLine(Devide);
+
+            int A = -5 + 8 * 6;
+            Console.WriteLine("a. " +A);
+            int B = ((55 + 9) % 9);
+            Console.WriteLine("b. " + B);
+            int C = 20 + -3 * 5 / 8;
+            Console.WriteLine("c. " + C);
+            int D = (5 + 15 / 3 * 2 - 8 % 3);
+            Console.WriteLine("d. " + D);
+
+            int f = 11;
+            int g = 11;
+            int addition = f + g;
+            int multiply = f * g;
+            int subtract = f - g;
+            int divide = f / g;
+            int remainder = f % g;
+            Console.WriteLine("addition  is "+ addition);
+            Console.WriteLine("multiply  is " + multiply);
+            Console.WriteLine("subtract  is "+subtract);
+            Console.WriteLine("dividtion  is " + divide);
+            Console.WriteLine("remainder  is " + remainder);
+
+            double j = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+            Console.WriteLine(j);
+
+
+            double Width = 5.5;
+            double Height = 8.5;
+            double area = Width * Height;
+            double Perimeter = 2 * (Width + Height);
+            Console.WriteLine("Area is 5.5*8.5 = " + area);
+            Console.WriteLine("Perimeter is 2*(5.5+8.5)=" + Perimeter);
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+    }
+git add.
+commit
+push
+
---
+++ lab2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace LabTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //task1
+            Console.WriteLine("Task 1");
+            Console.WriteLine();
+            Console.Write("Print  even numbers from 1 to 10 is : ");
+            for (int i = 1; i <= 200; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(" " + i);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //task2 
+            Console.WriteLine("Task 2");
+            Console.WriteLine();
+            int x = 0;
+            for (int i = 0; i < 4; i++)
+            {
+
+                for (int j = 0; j <= 5; j++)
+                {
+                    x = i + j;
+                    if (x % 2 == 0)
+                    {
+                        Console.Write("x");
+                    }
+                    else
+                    {
+                        Console.Write("0");
+                    }
+
+
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+            //task3
+            Console.WriteLine("Task 3");
+            Console.WriteLine();
+            for (int i = 1; i <= 5; i++)
+            {
+                x = i;
+
+                for (int j = 0; j <= 4; j++)
+                {
+                    Console.Write(x + j);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
