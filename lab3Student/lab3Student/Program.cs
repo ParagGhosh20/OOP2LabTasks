6766feeee994da3c32fff287f66855e923356557
ParagGhosh20 <paragghosh2@gmail.com>
2020-10-31 10:32:10 +0600

    lab3

---
+++ lab3Student/lab3Student/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using System;
+
+namespace labTask3
+{
+    class TestStudent
+    {
+        public static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.insert("Kaushik", "19-40034-1", "CS", 3.50);
+            s1.ShowInfo();
+            Triangle t1 = new Triangle();
+            t1.triangle(10, 10, 10);
+            t1.display();
+            t1.TestTriangle();
+        }
+    }
+}
---
+++ lab3Student/lab3Student/student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Student
+{
+    private String name;
+    private String id;
+    private String department;
+    private double cgpa;
+    public void insert(String Name, String Id, String Department, double Cgpa)
+    {
+        this.name = Name;
+        this.id = Id;
+        this.department = Department;
+        this.cgpa = Cgpa;
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine("Name "+ name);
+        Console.WriteLine("Id " + id);
+        Console.WriteLine("Department " + department);
+        Console.WriteLine("Cgpa " + cgpa);
+    }
+
+
+}
