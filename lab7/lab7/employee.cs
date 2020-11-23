41d1ad036ff7ea95ca9cd90eb47235c3edc8a53f
ParagGhosh20 <paragghosh2@gmail.com>
2020-11-23 11:30:16 +0600

    lab7

---
+++ lab7/lab7/employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab7
+{
+    class Employee
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string id;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public Employee()
+        {
+        }
+        public Employee(string id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Id: " + id);
+        }
+
+    }
+}
---
+++ lab7/lab7/savingsAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab7
+{
+    class SavingsAccount : Account
+    {
+        public SavingsAccount() { }
+        public SavingsAccount(string accNo, string accName, double balance) : base(accNo, accName, balance)
+        {
+
+        }
+        public override void Withdraw(double amount)
+        {
+            if (balance - amount >= 500)
+            {
+                balance -= amount;
+            }
+            else
+                Console.WriteLine("Insufficient Balance");
+        }
+    }
+}
