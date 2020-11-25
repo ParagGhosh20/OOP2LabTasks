06d1fc853fde4c9c35a7119e5b86aa1f548b8111
ParagGhosh20 <paragghosh2@gmail.com>
2020-11-25 10:08:28 +0600

    lab

---
+++ CalculatorFInal/CalculatorFInal/ScientificCalculatorInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Interface_1
+{
+    interface ScientificCalculatorInterface
+    {
+        int XtoY(int x, int y);
+
+    }
+}
+
